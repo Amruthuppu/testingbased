122fa2455009aafaba5917b41df70939acd003b0
934978 <934978@LTIN123456.cts.com>
2024-11-18 19:03:05 +0530

    commit2

---
+++ sample/src/main/java/sample/calculate.java
@@ -0,0 +1,27 @@
+package sample;
+
+
+public class calculate {
+
+	
+		// TODO Auto-generated method stub
+		int a;
+		int b;
+		int sum()
+		{
+			return (a+b);		
+			}
+		
+			public static void main(String[] args) 
+			{
+				calculate c1= new calculate();
+						c1.a=1000;
+						c1.b=300;
+						int r=c1.sum();
+						System.out.println(r);
+			
+				
+		}
+	
+
+}
